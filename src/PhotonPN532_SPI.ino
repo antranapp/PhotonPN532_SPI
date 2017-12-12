11d9822336fdb1a57abcbbf4422abdade79b0ca6
Binh An Tran <tran.binhan@gmail.com>
2017-12-12 23:21:37 +0100

    first commit

---
+++ src/PhotonPN532_SPI.ino
@@ -0,0 +1,36 @@
+
+#include "PN532_SPI.h"
+#include "PN532.h"
+#include "NfcAdapter.h"
+#include "Grove_LCD_RGB_Backlight.h"
+
+#define SS A2
+
+STARTUP(WiFi.selectAntenna(ANT_EXTERNAL));
+SYSTEM_THREAD(ENABLED);
+
+rgb_lcd lcd;
+
+PN532_SPI pn532_spi(SPI, SS);
+NfcAdapter nfc = NfcAdapter(pn532_spi);
+
+void setup(void) {
+    Serial.begin(9600);
+    Serial.println("NDEF Reader");
+
+    lcd.begin(16, 2);
+    lcd.setRGB(100, 100, 100);
+    lcd.print("hello, world!");
+
+    nfc.begin();
+}
+
+void loop(void) {
+    Serial.println("\nScan a NFC tag\n");
+    if (nfc.tagPresent())
+    {
+        NfcTag tag = nfc.read();
+        tag.print();
+    }
+    delay(5000);
+}
